281e1bf09b327a3a784b0a0a5d5a04d5ecdce723
Cihan ÇALLI <cihan.callii@gmail.com>
2022-04-06 21:22:14 +0300

    * .gitignore: First Commit
    * FileUploadToFirebaseStorage.sln:
    * Startup.cs:
    * Program.cs:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Error.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * launchSettings.json:
    * LICENSE.txt:
    * jquery.js:
    * FileUploadToFirebaseStorage.csproj:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ FileUploadToFirebaseStorage/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FileUploadToFirebaseStorage.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Firebase.Auth;
+using System.Threading;
+using Firebase.Storage;
+
+namespace FileUploadToFirebaseStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static string apiKey = "API_KEY";
+        private static string Bucket = "APP_ID.appspot.com";
+        private static string AuthEmail = "MAİL";
+        private static string AuthPassword = "PASSWORD";
+
+        private readonly IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            IFormFile fileupload = file;
+            FileStream fs = null;
+
+            var fileName = Path.GetFileName(fileupload.FileName);
+            var fileExtension = Path.GetExtension(fileName);
+            var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
+            if (fileupload.Length > 0)
+            {
+                //Upload the file to firebase
+                string foldername = "firebaseFiles";
+                string path = Path.Combine(_env.WebRootPath, $"images/{foldername}");
+
+
+
+                if (Directory.Exists(path))
+                {
+                    using (fs = new FileStream(Path.Combine(path, fileupload.FileName), FileMode.Create))
+                    {
+                        await fileupload.CopyToAsync(fs);
+                    }
+                    fs = new FileStream(Path.Combine(path, fileupload.FileName), FileMode.Open);
+                }
+                else
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //Firebase uploading stuff
+                var auth = new FirebaseAuthProvider(new FirebaseConfig(apiKey));
+                var a = await auth.SignInWithEmailAndPasswordAsync(AuthEmail, AuthPassword);
+
+                // you can use CancellationTokenSource to cancel the upload midway
+                var cancellation = new CancellationTokenSource();
+
+                var upload = new FirebaseStorage(
+                    Bucket,
+                    new FirebaseStorageOptions
+                    {
+                        AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                        ThrowOnCancel = true
+                    })
+                    .Child("assets")
+                    .Child($"{newFileName}")    //.{Path.GetExtension(fileupload.FileName).Substring(1)}
+                    .PutAsync(fs, cancellation.Token);
+
+                try
+                {
+                    ViewBag.link = await upload;
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"***** {ex} *****");
+                    throw;
+                }
+
+            }
+            return View();
+        }
+    }
+}
